fe9892a77e1604e9cd910ffe75d1289337bdd8f0
ubtXertion <3357099254@qq.com>
2026-05-03 11:09:34 +0800

    first commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xertion/Code/Tiny_KV
-# Build directory: /home/xertion/Code/Tiny_KV/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/my_stl-build")
-subdirs("tests")
--- build/_deps/my_stl-subbuild/CMakeFiles/my_stl-populate.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for my_stl-populate.
-# This may be replaced when dependencies are built.
